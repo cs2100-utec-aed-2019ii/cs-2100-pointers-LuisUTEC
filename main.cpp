9ff8cc9af54cbb26ddc5844673cd2ee6c7effb96
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2019-08-22 17:45:41 -0500

    hasta 16

--- main.cpp
+++ main.cpp
@@ -6,5 +6,28 @@ void ejercicio_x (int, int) {
 }
 
 int main (int, char * []){
+    //1. 1 2 3 4 5 6 7 8 9 10
+    //2. 7 5 10
+    //3. 
+    //4. 4   
+    //5. Si da error porque el puntero y a lo que esta apuntando deben ser del mismo tipo de variable.
+    //6. void swap(int *x, int *y){int temp = *x ;*x = *y; *y = temp; return;} 
+    //7. 
+    //8. 0 8
+    //9. 8 15
+    //10. 8
+    /*11. 
+        int a
+        int *ptr = a;
+        std::cout << a << " " << ptr << " " << &ptr;*/
+    //12. 13 13 &index(la direccion de index)
+    //13. 7 5 10
+    //14. 4 1
+    /*15.
+    int suma(int *a, int* b){
+        return *a + *b;
+    }
+    
+    
     return 1;
 }
